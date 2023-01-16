7b60b112ddfc9f68f77a356d145263f99b5250a9
Alex M <alexm0717@hotmail.com>
2023-01-15 23:26:53 -0800

    Coverage at 100

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::RecipesController < ApplicationController
 
   def index
+
     CountryFacade.random_c
     recipes = RecipesFacade.get_recipes(params[:country])
     render json: RecipesSerializer.new(recipes)
   end
-end
+  end
--- spec/requests/spec/recipes/index_spec.rb
+++ spec/requests/spec/recipes/index_spec.rb
@@ -8,14 +8,19 @@ RSpec.describe 'recipes#index', :vcr do
         get "/api/v1/recipes?country=#{country}"
 
         expect(response).to be_successful
-
         result = JSON.parse(response.body,symbolize_names: true)
-
         expect(result).to have_key(:data)
-
-        data = result[:data]
         expect(result[:data]).to be_an Array
-
+        expect(result[:data].first).to be_a(Hash)
+        expect(result[:data].first).to have_key(:id)
+        expect(result[:data].first[:id]).to eq(nil)
+        expect(result[:data].first).to have_key(:type)
+        expect(result[:data].first[:type]).to eq("recipes")
+        expect(result[:data].first).to have_key(:attributes)
+        expect(result[:data].first[:attributes][:id]).to eq(nil)
+        expect(result[:data].first[:attributes][:title]).to eq("Avocado from Mexico, Orange and Watercress Salad. Adapted from Chef Richard Sandoval, New York, Mexico and Dubai")
+        expect(result[:data].first[:attributes][:url]).to eq("https://food52.com/recipes/5794-avocado-from-mexico-orange-and-watercress-salad-adapted-from-chef-richard-sandoval-new-york-mexico-and-dubai")
+        expect(result[:data].first[:attributes][:country]).to be_a(String)
       end
     end
   end
