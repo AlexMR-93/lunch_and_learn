be1deb8eed43535d4851d6b9eec475f6d857261f
Alex M <alexm0717@hotmail.com>
2023-01-18 04:23:32 -0800

    sad path testing favorite index

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -7,14 +7,17 @@ class Api::V1::FavoritesController < ApplicationController
       existing_user.save
       render json: {success: "Favorite added successfully"}, status: 201
     else
-      render json: {error: "Bad Request. User Not Valid."}, status: 404
+      render json: {error: "Bad Request. Api keys do not match."}, status: 404
     end
   end
   def index
-    if existing_user.api_key == params[:api_key]
+    if !existing_user.nil?
     render json: FavoriteSerializer.new(existing_user.favorites)
+    else
+      render json: {error: "Bad Request. User Not Valid."}, status: 404
     end
   end
+
 
   private
 
--- spec/requests/spec/favorites/index_spec.rb
+++ spec/requests/spec/favorites/index_spec.rb
@@ -27,9 +27,36 @@ describe 'happy path #index' do
       get "/api/v1/favorites?api_key=#{api_key1}"
 
       data = JSON.parse(response.body,symbolize_names: true)
-      binding.pry
       expect(data).to be_a(Hash)
+      expect(data[:data].first).to be_a(Hash)
+      expect(data[:data].first[:id]).to be_a(String)
+      expect(data[:data].first[:type]).to be_a(String)
+      expect(data[:data].first[:type]).to eq("favorite")
+      expect(data[:data].first[:attributes]).to be_a(Hash)
+      expect(data[:data].first[:attributes][:recipe_title]).to be_a(String)
+      expect(data[:data].first[:attributes][:recipe_title]).to eq("Recipe: Egyptian Tomato Soup")
+
+    end
+  end
+end
+describe 'sad path #index' do
+  describe 'when a request is made to POST /api/v1/favorites/api_key ' do
+    it 'returns an error code 404 ', :vcr do
+
+      test_user2 = User.create!(name: "Alex Mora", email: "AAAAAAAA@gmail.com", api_key: SecureRandom.hex(27))
+      test_user2.favorites.create!({
+                                     country: "thailand",
+                                     recipe_link: "https://www.tastingtable.com/.....",
+                                     recipe_title: "Crab Fried Rice (Khaao Pad Bpu)"
+                                   })
+      get "/api/v1/favorites?api_key=77777999999"
+
+      data = JSON.parse(response.body,symbolize_names: true)
+      expect(data).to be_a(Hash)
+      expect(data[:error]).to eq("Bad Request. User Not Valid.")
+      expect(response).to have_http_status(404)
     end
   end
 end
 end
+
